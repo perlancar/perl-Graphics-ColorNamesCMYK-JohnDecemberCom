8d8647e25464f771dd0f19bd0af9c11be8d01332
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2024-05-06 16:29:47 +0700

    Initial commit

---
+++ lib/Graphics/ColorNamesCMYK/JohnDecemberCom.pm
@@ -0,0 +1,34 @@
+package Graphics::ColorNamesCMYK::JohnDecemberCom;
+
+use strict;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+# BEGIN_CODE
+#use HashData::Color::CMYK::JohnDecemberCom;
+#
+#print 'our $NAMES_CMYK_TABLE = {', "\n";
+#
+#my $hd = HashData::Color::CMYK::JohnDecemberCom->new;
+#
+#while ($hd->has_next_item) {
+#    my $row = $hd->get_next_item;
+#    my ($c, $m, $y, $k) = split /,/, $row->[1];
+#    print sprintf "  '%s' => 0x%08x, # %d,%d,%d,%d\n", $row->[0], ($c * 256**3 + $m * 256**2 + $y * 256 + $k), $c, $m, $y, $k;
+#}
+#
+#print "};\n\n";
+# END_CODE
+
+1;
+# ABSTRACT: CMYK color names from johndecember.com
+
+=head1 DESCRIPTION
+
+
+=head1 SEE ALSO
+
+Other C<Graphics::ColorNamesCMYK::*> modules.
